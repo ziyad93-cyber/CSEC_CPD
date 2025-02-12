980d34eaaba740cef781867d5c9474828bd6d9a1
ziyad93-cyber <ziyadayub93@gmail.com>
2025-02-13 00:11:04 +0300

    Create games.cpp

---
+++ games.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a,count=0;
+  cin>>a;
+  int x[a],y[a];
+  for(int i=0;i<a;i++){
+      cin>>x[i];
+      cin>>y[i];
+  }
+  for(int i=0;i<a;i++){
+      for(int j=0;j<a;j++){
+          if(x[i]==y[j]){
+              count++;
+          }
+      }
+  } 
+  cout<<count;
+return 0;
+}
